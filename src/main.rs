d523f9962589b7813aba9ca03b77c04f9b92c28e
Filipe Santos <filipesantos1999@hotmail.com>
2023-02-18 19:06:23 +0000

    Add TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    
+// rust
+use std::net::TcpListener;
+
+
+fn main () -> std::io::Result<()> {
+    // let listener = TcpListener::bind("localhost:8080").unwrap_or_else(|error| {
+    //     println!("Error: {:?}", error);
+    // });
+
+    let listener = TcpListener::bind("localhost:8080")?;
+    println!("Listening: {:?}", listener.local_addr()?);
+
+    for data_incoming in listener.incoming() {
+
+    }
+    Ok(())
 }
