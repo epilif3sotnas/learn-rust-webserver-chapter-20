9bd0988bd0729935db35aab7433b413b9d377d60
Filipe Santos <filipesantos1999@hotmail.com>
2023-02-23 07:53:43 +0000

    Add some TODO

--- src/main.rs
+++ src/main.rs
@@ -31,9 +31,11 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+// ### TODO - Think about send back errors ###
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
+    // ### TODO - robust line unwrap ###
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|line| line.unwrap())
