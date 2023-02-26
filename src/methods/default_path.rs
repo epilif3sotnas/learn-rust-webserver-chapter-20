e19c841799cbbb34921559d1adaae546d7a8488a
Filipe Santos <filipesantos1999@hotmail.com>
2023-02-26 14:44:41 +0000

    Add unit tests for default_path file

--- src/methods/default_path.rs
+++ src/methods/default_path.rs
@@ -36,21 +36,49 @@ mod tests {
 
     #[test]
     fn unit__get_method__valid() {
+        let message = String::from("You did a GET\nHello!\nHi from Rust programming language.");
+        let size = message.len();
 
+        let result = get_method();
+    
+        let expected = format!("HTTP/1.1 200 OK\r\nContent-Length: {size}\r\n\r\n{message}");
+
+        assert_eq!(result, expected);
     }
 
     #[test]
     fn unit__post_method__valid() {
+        let message = String::from("You did a POST\nHello!\nHi from Rust programming language.");
+        let size = message.len();
 
+        let result = post_method();
+    
+        let expected = format!("HTTP/1.1 200 OK\r\nContent-Length: {size}\r\n\r\n{message}");
+
+        assert_eq!(result, expected);
     }
 
     #[test]
     fn unit__put_method__valid() {
+        let message = String::from("You did a PUT\nHello!\nHi from Rust programming language.");
+        let size = message.len();
 
+        let result = put_method();
+    
+        let expected = format!("HTTP/1.1 200 OK\r\nContent-Length: {size}\r\n\r\n{message}");
+
+        assert_eq!(result, expected);
     }
 
     #[test]
     fn unit__delete_method__valid() {
+        let message = String::from("You did a DELETE\nHello!\nHi from Rust programming language.");
+        let size = message.len();
 
+        let result = delete_method();
+    
+        let expected = format!("HTTP/1.1 200 OK\r\nContent-Length: {size}\r\n\r\n{message}");
+
+        assert_eq!(result, expected);
     }
 }
